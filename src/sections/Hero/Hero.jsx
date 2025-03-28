57c7c19c259380bed23a31a6c6755ce4285c89c6
PC <jupallyyashwanth376@gmail.com>
2025-03-28 13:29:51 +0530

    small changes

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -9,7 +9,7 @@ import linkedinLight from '../../assets/linkedin-light.svg';
 import linkedinDark from '../../assets/linkedin-dark.svg';
 import githubLight from '../../assets/github-light.svg';
 import githubDark from '../../assets/github-dark.svg';
-import cv from '../../assets/Yashwanth Jupally - Resume SE.pdf';
+import cv from '../../assets/Yashwanth SDE.pdf';
 import { useTheme } from '../../common/ThemeContext';
 
 function Hero() {
--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -15,25 +15,25 @@ function Skills() {
       <h1 className={styles.sectionTitle}>About</h1>
       <p className={styles.about}>I am Yashwanth, a graduate in Electrical Engineering and a passionate web application developer. I am proficient in building Java, Spring Boot and React-based applications. I love connecting with people and exploring new things. My hobbies include music, photography, and miniature craft. Below is my primary tech stack</p>
       <div className={styles.skillssection}>
-        <a href='https://github.com/yashwanthjupally/Employee-Management-System' target='_blank'>
+        <a>
           <img className={styles.skillImg} src={java} alt='Java logo'/>
         </a>
-        <a href='https://github.com/yashwanthjupally/Employee-Management-System' target='_blank'>
+        <a>
           <img className={styles.skillImg} src={html} alt='HTML logo'/>
         </a>
-        <a href='https://github.com/yashwanthjupally/Employee-Management-System' target='_blank'>
+        <a>
           <img className={styles.skillImg} src={css} alt='CSS logo'/>
         </a>
-        <a href='https://github.com/yashwanthjupally/Employee-Management-System' target='_blank'>
+        <a>
           <img className={styles.skillImg} src={js} alt='JavaScript logo'/>
         </a>
-        <a href='https://github.com/yashwanthjupally/Employee-Management-System' target='_blank'>
+        <a>
           <img className={styles.skillImg} src={spring} alt='Spring logo'/>
         </a>
-        <a href='https://github.com/yashwanthjupally/Employee-Management-System' target='_blank'>
+        <a>
           <img className={styles.skillImg} src={react} alt='React logo'/>
         </a>
-        <a href='https://github.com/yashwanthjupally/Employee-Management-System' target='_blank'>
+        <a>
           <img className={styles.skillImg} src={sql} alt='MySQL logo'/>
         </a>
       </div>
